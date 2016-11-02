f7567c5769b48ce27149291199797edf69bcbf9f
M J Stanway <m.j.stanway@alum.mit.edu>
2016-11-02 12:40:57 -0700

    add TDD example with first test

---
+++ mod.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# example python module demonstrating test-driven development
+# (see http://katyhuff.github.io/python-testing/09-tdd.html)
+
+
+
+
+
+# Tests:
+def test_std1():
+    obs = std([0.0, 2.0])
+    exp = 1.0
+    assert obs = exp
