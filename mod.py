0b85bbf4678c49ffb5fbcc005c82133ea5dceae6
M J Stanway <m.j.stanway@alum.mit.edu>
2016-11-02 12:48:21 -0700

    add more tests where the answer is not one

--- mod.py
+++ mod.py
@@ -14,3 +14,17 @@ def test_std1():
     obs = std([0.0, 2.0])
     exp = 1.0
     assert obs == exp
+
+def test_std2():
+    # Test the fiducial case when we pass an empty list.
+    obs = std([])
+    exp = 0.0
+    assert obs == exp
+
+def test_std3():
+    # Test a real case where the answer is not one.
+    obs = std([0.0, 4.0])
+    exp = 2.0
+    assert_equal(obs, exp)
+
+
